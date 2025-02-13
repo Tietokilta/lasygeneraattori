bf7fdd288bb4bee02d83863a55aa8592cf2f3eb4
Olli Nurminen <olli.nurminen@aalto.fi>
2025-02-13 18:52:32 +0200

    Create header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className="bg-slate-400 w-fill text-center h-10">Header???</div>;
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import SongList from "./components/SongList";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <SongList />
     </div>
   );
